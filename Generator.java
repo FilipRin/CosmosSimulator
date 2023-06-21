1386ec64c422c3fe42a80022b1d88727113fb2bc
Filip Rinkovec <69078204+FilipRin@users.noreply.github.com>
2023-06-21 15:23:49 +0200

    Add files via upload

--- Generator.java
+++ Generator.java
@@ -9,6 +9,7 @@ public class Generator extends Thread {
 
 	private Svemir svemir;
 	Kometa kometa;
+	Planeta planeta;
 	private boolean work;
 	
 	public Generator(Svemir svemir) {
@@ -25,14 +26,40 @@ public class Generator extends Thread {
 					}
 				}
 				sleep(900);
-				
-				int x= (int) (Math.random()*201);
-				int y=0;
-				int r=(int) (Math.random()*21+10);
-				//System.out.println("x "+x+" y "+y+" r "+r);
-				kometa=new Kometa(x,y,Color.GRAY,r);
-				kometa.setParam();
-				svemir.dodajNebeskoTelo(kometa);
+				double check=Math.random()*10;
+				if(check>=2.5) {
+					int x= (int) (Math.random()*201);
+					int y=0;
+					int r=(int) (Math.random()*21+10);
+					//System.out.println("x "+x+" y "+y+" r "+r);
+					kometa=new Kometa(x,y,Color.GRAY,r);
+					kometa.setParam();
+					svemir.dodajNebeskoTelo(kometa);
+				}
+				else {
+					int x= (int) (Math.random()*201);
+					int y=0;
+					int r=(int) (Math.random()*21+10);
+					double color=Math.random()*10;
+					Color col=null;
+					if(color<2.5) {
+						col=Color.GREEN;
+					}
+					else if(color>=2.5 & color<5) {
+						col=Color.BLUE;
+					}
+					else if(color>=5 & color<7.5) {
+						col=Color.YELLOW;
+					}
+					else {
+						col=Color.RED;
+					}
+					double ring=Math.random()*10;
+					
+					planeta=new Planeta(x,y,col,r);
+					planeta.ring(ring);
+					svemir.dodajNebeskoTelo(planeta);
+				}
 				
 			}
 		} catch (InterruptedException e) {}
@@ -40,6 +67,10 @@ public class Generator extends Thread {
 	
 	public synchronized void go() {
 		work=true;
+		notify();
+	}
+	public synchronized void pause() {
+		work=false;
 		notify();
 	}
 	
